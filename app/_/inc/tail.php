78b3028afabdec21293c518768edadd2d7cfa8eb
Mike Lynch <mike@theinfluence.net>
2015-05-30 20:23:26 -0400

    updated to include slider on page 1

--- app/_/inc/tail.php
+++ app/_/inc/tail.php
@@ -1,7 +1,7 @@
 
 
         <!-- build:js(app) /_/js/main.js -->
-        <script src="/_/bower_components/jquery/jquery.js"></script>
+
         <script src="/_/js/functions.js"></script>
         <script src="/_/js/validation.js"></script>
         <!-- endbuild -->
